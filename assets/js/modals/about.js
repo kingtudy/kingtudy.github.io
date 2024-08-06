3652b35105b10e5f2b798bcd23a9685ff66f67f7
king <tudor1103@gmail.com>
2024-08-06 22:27:35 +0300

    finish about me section

--- assets/js/modals/about.js
+++ assets/js/modals/about.js
@@ -1,13 +1,11 @@
 
 
 const aboutContent = [
-    '<h6>WELCOME! What do you need to know?</h6>' +
-    '<br>' +
-    '<a class="index-btn s-font" onclick="jumpToPage(0);">1. Introduction</a>' +
-    '<a class="index-btn s-font" onclick="jumpToPage(3);">2. General overview</a>' +
-    '<a class="index-btn s-font" onclick="jumpToPage(21);">3. Object & camera controls</a>' +
-    '<a class="index-btn s-font" onclick="jumpToPage(24);">4. Shortcut keys</a>' +
-    '<a class="index-btn s-font" onclick="jumpToPage(25);">5. Credits</a>',
+    '<div style="height: 100%;" class="display-content-container">' +
+    '<div class="display-content">' +
+    '<h4 class="white subnautica-content-font">I\'m a highly skilled and motivated web developer with 5 years of experience in designing visually appealing and user-friendly websites. <br>I possess strong problem-solving abilities and a keen eye for detail. <br>I have an engineering degree, and I\'m fluent in English(C1 Academic IELTS exam). <br><br>Right now, I\'m looking to further improve myself and my skills by learning new modern web developer tools such as React, Next.js, Vue and so on. I also have prior knowledge with C, C++, Python and Java. I\'m eager to learn and adjust in order to meet the demands of the task at hand, including the willingness to work beyond regular hours when required. I take pride in the work that I do and look into further expanding my knowledge on various technological fields.</h4>' +
+    '</div>'+
+    '</div>',
 ];
 
 const aboutBtn = $("#aboutBtn");
